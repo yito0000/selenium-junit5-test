f43ab4169a8819bdc365e9b389c784575c0d2326
yito <fiv.e2b3s@gmail.com>
2019-03-03 18:23:00 +0900

    init

---
+++ src/main/java/com/example/seleniumjunit5test/SeleniumJunit5TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.seleniumjunit5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumJunit5TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumJunit5TestApplication.class, args);
+	}
+
+}
