302e861b69c58df3616769ef06c6c78cf748b57f
yito <fiv.e2b3s@gmail.com>
2019-03-31 18:02:25 +0900

    画面開くようになったのでコミット

--- src/main/java/com/example/seleniumjunit5test/controller/AppController.java
+++ src/main/java/com/example/seleniumjunit5test/controller/AppController.java
@@ -1,15 +1,36 @@
 package com.example.seleniumjunit5test.controller;
 
+import com.example.seleniumjunit5test.model.AppModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 public class AppController {
 
-    @GetMapping(value = "/input")
-    public ModelAndView showInput() {
+    @GetMapping(path = "/input")
+    public ModelAndView showInput(ModelAndView modelAndView) {
+        modelAndView.setViewName("input");
+        AppModel appModel = new AppModel();
+        modelAndView.addObject("appModel", appModel);
+        return modelAndView;
+    }
 
-        return new ModelAndView("views/input");
+    @PostMapping(path = "/confirm")
+    public String redirectConfirm(@ModelAttribute("appModel") AppModel appModel, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("appModel", appModel);
+        return "redirect:/confirm";
+    }
+
+    @GetMapping(path = "/confirm")
+    public ModelAndView showConfirm(@ModelAttribute("appModel") AppModel appModel) {
+        System.out.println(appModel);
+        return new ModelAndView("confirm");
     }
 }
---
+++ src/main/java/com/example/seleniumjunit5test/model/AppModel.java
@@ -0,0 +1,10 @@
+package com.example.seleniumjunit5test.model;
+
+import lombok.Data;
+
+@Data
+public class AppModel {
+
+    private String name;
+
+}
